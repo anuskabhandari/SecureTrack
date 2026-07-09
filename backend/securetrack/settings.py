d13bfb7af403b2f13b966af7aead014c817e8d5d
anuskabhandari <bhandarirekha652@gmail.com>
2026-07-09 22:19:49 +0545

    refactor:update github workflow

--- backend/securetrack/settings.py
+++ backend/securetrack/settings.py
@@ -93,11 +93,11 @@ WSGI_APPLICATION = 'securetrack.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "NAME": os.getenv("DB_NAME", "secure"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "postgres"),
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
